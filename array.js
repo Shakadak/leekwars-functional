3d52c0eec6950e492d81feea19fb6fac7aacbff3
shakadak <nathanael.pineau@gmail.com>
2016-08-26 16:51:11 +0200

    javadoc array

--- array.js
+++ array.js
@@ -1,7 +1,10 @@
 include("functional");
 include("bool");
 
-// --- aFoldL : (b -> a -> b) -> b -> Array a -> b
+
+/**
+* aFoldL : (b -> a -> b) -> b -> Array a -> b
+*/
 function aFoldL(@f) { return function(@b) { return function(@as) {
 	/*var ret = b;
 	for (var a in as) {
@@ -12,12 +15,16 @@ function aFoldL(@f) { return function(@b) { return function(@as) {
 	return arrayFoldLeft(as, uncurry2(f), b);
 };};};
 
-// ---   aFoldR : (a -> b -> b) -> b -> Array a -> b
+/**
+* aFoldR : (a -> b -> b) -> b -> Array a -> b
+*/
 function aFoldR(@f) { return function(@b) { return function(@as) {
 	return arrayFoldRight(as, uncurry2(f), b);
 };};}
 
-// --- aFilter : (a -> Bool) -> Array a -> Array a
+/**
+* aFilter : (a -> Bool) -> Array a -> Array a
+*/
 function aFilter(@p) { return function(@xs) {
     var tmp = [];
     for (var x in xs) {
@@ -27,12 +34,16 @@ function aFilter(@p) { return function(@xs) {
     return @tmp;
 };};
 
-// ---   aMap : (a -> b) -> Array a -> Array b
+/**
+* aMap : (a -> b) -> Array a -> Array b
+*/
 function aMap(@f){ return function(@xs) {
 	return arrayFoldLeft(xs, function(@acc, @x){push(acc, f(x));return @acc;}, []);
 };};
 
-// ---   aConcatMap : (a -> Array b) -> Array a -> Array b
+/**
+* aConcatMap : (a -> Array b) -> Array a -> Array b
+*/
 function aConcatMap(@f) { return function(@xs) {
 	var tmp = [];
 	for (var x in xs) {
@@ -42,17 +53,25 @@ function aConcatMap(@f) { return function(@xs) {
 	return @tmp;
 };}
 
+/**
+* aConcat : Array (Array a) -> Array a
+*/
 function aConcat(@xss) {
 	return arrayFoldRight(xss, arrayConcat, []);
 }
 
+/**
+* aAppend : Array a -> Array a -> Array a
+*/
 function aAppend(@xs) {return function(@ys) {
 	//return arrayFoldLeft(ys, function(@acc, @y){push(acc, y);return @acc;}, xs);
 	//return arrayFoldRight(xs, function(@x, @acc){unshift(acc, x);return @acc;}, ys);
 	return @(xs + ys);
 };}
 
-// ---   aApply : Array (a -> b) -> Array a -> Array b
+/**
+* aApply : Array (a -> b) -> Array a -> Array b
+*/
 function aApply(@fs) { return function(@xs) {
 	var ret = [];
 	for (var f in fs) {
@@ -62,7 +81,9 @@ function aApply(@fs) { return function(@xs) {
 	return @ret;
 };}
 
-// ---   aIntersection : Array a -> Array a -> Array a
+/**
+* aIntersection : Array a -> Array a -> Array a
+*/
 function aIntersection(@xs) { return function(@ys) {
     if (count(ys) < count(xs)) {return aIntersection(ys)(xs);}
     var tmp = [];
@@ -71,7 +92,9 @@ function aIntersection(@xs) { return function(@ys) {
     return aFilter(assocLookup(tmp))(ys);
 };}
 
-// ---   aRelativeComplement : Array a -> Array a -> Array a
+/**
+* aRelativeComplement : Array a -> Array a -> Array a
+*/
 function aRelativeComplement(@xs) { return function(@ys) {
     var tmp = [];
     for (var y in ys) {
@@ -79,14 +102,20 @@ function aRelativeComplement(@xs) { return function(@ys) {
     return aFilter(negate(assocLookup(tmp)))(xs);
 };}
 
-// ---   isEmpty : Array a -> Bool
+/**
+* isEmpty : Array a -> Bool
+*/
 function isEmpty(@xs) { return count(xs) === 0; }
 
-// ---   isNonEmpty : Array a -> Bool
+/**
+* isNonEmpty : Array a -> Bool
+*/
 function isNonEmpty(@xs) { return count(xs) !== 0; }
 
 
-// ---   getKeys : Assoc a b -> Array a
+/**
+* getKeys : Assoc a b -> Array a
+*/
 function getKeys(xs) {
     var ks = [];
     for (var k : var x in xs) {
@@ -95,18 +124,24 @@ function getKeys(xs) {
     return @ks;
 }
 
-// --    pairKV : a -> b -> Assoc a b
+/**
+* pairKV : a -> b -> Assoc a b
+*/
 function pairKV(@k) { return function(@v) {
     var type = typeOf(k);
     return @[(type === TYPE_NUMBER || type === TYPE_STRING ? k : "" + k) : v];};}
 
-// ---   assocInsert : a -> b -> Assoc a b -> Assoc a b [mutating]
+/**
+* assocInsert : a -> b -> Assoc String b -> Assoc String b [mutating]
+*/
 function assocInsert(@k) { return function(@v) { return function(@xs) {
     xs["" + k] = v;
     return @xs;
 };};}
 
-// ---   assocLookup : Assoc a b -> a -> b
+/**
+* assocLookup : Assoc String b -> a -> b
+*/
 function assocLookup(@xs) { return function(@k) {
     return @xs["" + k];
 };}
