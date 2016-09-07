d520e3fbbf2c305ab20aa4ff97ca60fb0114ff44
shakadak <nathanael.pineau@gmail.com>
2016-09-07 16:52:00 +0200

    removed coercive equality and added notEqual, because it is what I needed.

--- bool.js
+++ bool.js
@@ -6,4 +6,9 @@ function negate(@p) { return function(@a) {return !p(a); };}
 /**
 * equal : a -> a -> Bool
 */
-function equal(@x) { return function(@y) { return x == y; };}
+function equal(@x) { return function(@y) { return x === y; };}
+
+/**
+* notEqual : a -> a -> Bool
+*/
+function notEqual(@x) { return function(@y) { return x !== y; };}
