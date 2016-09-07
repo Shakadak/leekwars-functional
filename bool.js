ea336c15241300926c449e2ad756bab1257a8c87
shakadak <nathanael.pineau@gmail.com>
2016-09-07 16:48:41 +0200

    curryed equal

--- bool.js
+++ bool.js
@@ -1,4 +1,9 @@
 /**
 * negate : (a -> Bool) -> a -> Bool
 */
-function negate(@p) { return function(@a) {return !p(a);};}
+function negate(@p) { return function(@a) {return !p(a); };}
+
+/**
+* equal : a -> a -> Bool
+*/
+function equal(@x) { return function(@y) { return x == y; };}
