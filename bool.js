a13ddfb78ae757fb228bd78d30546db67b650d50
shakadak <nathanael.pineau@gmail.com>
2016-09-17 00:57:48 +0200

    comparing

--- bool.js
+++ bool.js
@@ -12,3 +12,17 @@ function equal(@x) { return function(@y) { return x === y; };}
 * notEqual : a -> a -> Bool
 */
 function notEqual(@x) { return function(@y) { return x !== y; };}
+
+/**
+* comparing : (a -> b) -> (b -> b -> c) -> a -> a -> c
+*/
+function comparing(@f) { return function(@p) { return function(@x) { return function(@y) {
+	return p(f(x))(f(y));
+};};};}
+
+/**
+* comparingu : (a -> b) -> ((b, b) -> c) -> (a, a) -> c
+*/
+function comparingu(@f) { return function(@p) { return function(@x, @y) {
+	return p(f(x), f(y));
+};};}
