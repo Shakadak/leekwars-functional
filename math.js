26474a1f130bf50c10a21e9193bf54aee57be497
shakadak <nathanael.pineau@gmail.com>
2016-10-25 01:42:28 +0200

    division with default in case of 0'

--- math.js
+++ math.js
@@ -7,3 +7,11 @@ function add(@x) { return function(@y) { return x + y; };}
 * subTo : Num a => a -> a -> a
 */
 function subTo(@x) { return function(@y) { return y - x; };}
+
+/**
+* defaultDiv : Num a => a -> a -> a -> a
+*/
+function defaultDiv(@default) { return function(@x) { return function(@y) {
+	return y !== 0	? x / y
+					: default;
+};};}
